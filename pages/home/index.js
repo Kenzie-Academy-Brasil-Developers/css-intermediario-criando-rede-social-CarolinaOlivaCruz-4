8c1543a591c2ef7b6014d717c88154e1bd2aac73
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-09-26 08:09:17 -0300

    atualização modal

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,47 +1,31 @@
+
 function createModal() {
     const modalContainer = document.createElement('section')
     const contentContainer = document.createElement('div')
     const buttonContainer = document.createElement('div')
     const closeModal = document.createElement('button')
-    const divImg = document.createElement('div')
-    const figure = document.createElement('figure')
-    const img = document.createElement('img')
-    const userDate = document.createElement('div')
-    const nameUser = document.createElement('h3')
-    const jobUser = document.createElement('p')
-    const article = document.createElement('article')
-    const h2 = document.createElement('h2')
-    const p = document.createElement('p')
-
+    const divLoggedUser = document.querySelector('.div-logged-user')
+    const figure = document.querySelector('.figure')
+    const img = document.querySelector('.img')
+    const divUserDate = document.querySelector('.div-user-date')
+    const h3Name = document.querySelector('.h3-name')
+    const pJob = document.querySelector('.p-job')
+    const article = document.querySelector('.article-post')
+    const h2Title = document.querySelector('.h2-title')
+    const pArticle = document.querySelector('p-article')
 
     modalContainer.id = 'modal_container'
+
+    closeModal.id = 'close_modal'
     closeModal.innerText = 'X'
 
-    divImg.classList = 'div-logged-user'
-    img.scr = '../../assets/img/user1.svg'
-    img.alt = 'Foto do Usuário'
-
-    nameUser.innerText =
-
-
-    buttonContainer.appendChild(closeModal)
-    figure.appendChild(img)
-    divImg.appendChild(figure)
-    userDate.append(nameUser, jobUser)
-    article.appendChild(h2, p)
-    contentContainer.append(buttonContainer, divImg, article)
-    modalContainer.appendChild(contentContainer)
-}
-
-
-function createModal() {
-
-    const userPost = document.querySelector('.user-post')
-    const divLoggedUser = document.querySelector('.div-logged-user')
-    const figure = document.querySelector('.div-logged-user')
-    const img = document.querySelector('.div-logged-user')
-    const divUserDate = document.querySelector('.div-logged-user')
-    const h3 = document.querySelector('.div-logged-user')
-
     
+
+
+    buttonContainer.append(closeModal)
+    figure.append(img)
+    divUserDate.append(h3Name, pJob)
+    article.append(h2Title, pArticle)
+    divLoggedUser.append(figure, divUserDate, article)
+    contentContainer.append(buttonContainer, divLoggedUser)
 }
